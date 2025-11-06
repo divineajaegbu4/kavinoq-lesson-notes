597a6bbc666a15329cbe9ee4283f218bc681d329
Divine Ajaegbu <divineajaegbu4@gmail.com>
2025-11-06 19:46:12 +0100

    refactor the code using destructuring and rest operator

--- Month2-lesson-notes/real-world-promises.js
+++ Month2-lesson-notes/real-world-promises.js
@@ -1,64 +1,108 @@
-const getUser = () => {
-    const fetchUser = fetch("https://jsonplaceholder.typicode.com/users").then(res => res.json())
 
-    return new Promise(resolve => {
-   
-       setTimeout(async () => {
-        try {
-          const user = await fetchUser
-           resolve({message: "fetch user:", data: user})
-        }catch(err) {
-            console.log(err)
-        }
-            
-        }, 1000) 
-    })
-       
-}
+const getUser = async () => {
+    const response = await fetch("https://jsonplaceholder.typicode.com/users")
 
-const getPost = () =>{
-    const fetchPosts = fetch("https://jsonplaceholder.typicode.com/posts").then(res => res.json())
-    
-    return new Promise(resolve => {
-    setTimeout(async() => {
-        try {
-            const post = await fetchPosts
-            resolve({message: "fetch posts:", data: post})
-        }catch(err) {
-            console.log(err)
-        }
-           
-        }, 2000)
-     
-    })
-    
-}
+    const fetchUser = await response.json()
 
-const getComments = () => {
-    
-    const fetchComment = fetch("https://jsonplaceholder.typicode.com/comments").then(res => res.json())
+    const userTimeTaken = 1000;
 
-    return new Promise((_, reject) => {
-        setTimeout(async() => {
-            try {
-            const comments = await fetchComment
-            reject({message: "reject comment:", data: comments})
-            }catch(err) {
-                console.log(err)
+
+    const userPromise = new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if(response.status === 200) {
+                resolve({message: "user",  data: fetchUser})
+            }else {
+                reject("The response is not successful")
             }
-            }, 3000) 
+        }, userTimeTaken)
     })
+
+    return {userPromise, userTimeTaken}
+}
+       
+const getPost = async () => {
+    const response = await fetch("https://jsonplaceholder.typicode.com/posts")
+
+    const fetchPosts = await response.json();
+    
+    const postTimeTaken = 2000;
+
+    const postPromise =  new Promise((resolve, reject) => {
+    setTimeout(() => {
+         if(response.status === 200) {
+            resolve({message: "fetch posts:", fetchPosts})
+         }else {
+            reject("The status is not 200")
+         }
+            
+        }, postTimeTaken)
+    })  
+    
+    return {postPromise, postTimeTaken}
+}
+
+const getComments = async () => {
+    
+    const response = await fetch("https://jsonplaceholder.typicode.com/comments")
+
+    const fetchComment = await response.json()
+
+    const commentsTimeTaken = 3000;
+
+    const commentsPromise = new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if(response.status === 200) {
+               resolve({message: "reject comment:", data: fetchComment})
+            }else {
+                reject("The fetch post is not successful")
+            }
+            }, commentsTimeTaken) 
+    })
+
+    return {commentsPromise, commentsTimeTaken}
     
 }
- 
 
-getUser()
-.then(data => {
-    console.log(data)
-    return getPost()
-}).then(post => {
-    console.log(post)
-    return getComments()
-}).then(comment => {
-    console.log(comment)
-}).catch(err => console.log(err))
+
+const getData = async() => {
+try{
+    const {userPromise, userTimeTaken} =  await getUser();
+    const {postPromise, postTimeTaken} = await getPost();
+    const {commentsPromise, commentsTimeTaken} = await getComments();
+
+     const getTime = [userTimeTaken, postTimeTaken, commentsTimeTaken]
+
+
+    const getTotalTimeTaken = getTime.reduce((total, time) => {
+         return total += time;
+    }, 0)
+   
+    const [userData, postData, commentsData] = await Promise.all([userPromise, postPromise, commentsPromise])
+
+    console.log({...userData});
+    console.log({...postData});
+    console.log({...commentsData});
+    console.log("Total time taken:", getTotalTimeTaken);
+    
+   
+ }catch(err) {
+    console.log("Error:", err);
+ }
+
+}
+
+console.log(getData());
+
+
+// getUser()
+// .then(data => {
+//     console.log(data)
+//     return getPost()
+// }).then(post => {
+//     console.log(post)
+//     return getComments()
+// }).then(comment => {
+//     console.log(comment)
+// }).catch(err => console.log(err))
+
+
